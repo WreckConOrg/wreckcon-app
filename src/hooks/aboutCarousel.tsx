fdaa1eb12f90ac243bd5260a18a733b9feef258a
hdao35 <hdao35@gatech.edu>
2023-10-09 17:59:03 -0400

    fixed image transition and add more images

--- src/hooks/aboutCarousel.tsx
+++ src/hooks/aboutCarousel.tsx
@@ -3,9 +3,15 @@ import carousel1 from './../assets/about_carousel/1.jpg'
 import carousel2 from './../assets/about_carousel/2.jpg'
 import carousel3 from './../assets/about_carousel/3.jpg'
 import carousel4 from './../assets/about_carousel/4.jpg'
+import carousel5 from './../assets/about_carousel/5.jpg'
+import carousel6 from './../assets/about_carousel/6.jpg'
+import carousel7 from './../assets/about_carousel/4.jpg'
+import carousel8 from './../assets/about_carousel/5.jpg'
+import carousel9 from './../assets/about_carousel/6.jpg'
+import carousel10 from './../assets/about_carousel/6.jpg'
 
 const useCarousel = () => {
-    const images = [carousel1, carousel2, carousel3, carousel4]
+    const images = [carousel1, carousel2, carousel3, carousel4, carousel5, carousel6, carousel7, carousel8, carousel9, carousel10]
     let [timer, setTimer] = useState(0);
     let [currentIndex, setCurrentIndex] = useState(1);
     let [currentImage, setCurrentImage] = useState(images[currentIndex - 1]);
@@ -20,13 +26,16 @@ const useCarousel = () => {
             console.log(currentIndex);
             setCurrentImage(images[currentIndex]);
 
-            setTimer(0)
         }
         clearInterval(interval)
     }, 10);
 
+    useEffect(() => {
+        setTimer(0)
+    }, [currentImage])
+
     const imageStyle = {
-        opacity: timer < 2000 ? timer / 2000 : timer < 5000 ? 1 : 1 - (timer - 7000) / 7000, // Fade out for 2 seconds, then fade in for 2 seconds
+        opacity: timer < 2000 ? timer / 2000 : timer < 5000 ? 1 : 1 - (timer - 5000) / 2000, // Fade out for 2 seconds, then fade in for 2 seconds
     };
 
     return <>
