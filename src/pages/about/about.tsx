4d6cc14232847c7ce263a8ab7df882b33f592c90
WreckConOrg <gtwreckcon@gmail.com>
2023-10-25 00:09:16 -0400

    board games

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -34,7 +34,7 @@ export const About = (): JSX.Element => {
                 </div>
             </div>
             <div className="flex items-center justify-center h-fit space-x-[4vw]">
-                <ClubLogo logo={boardgames} clubLink='https://www.derivative-calculator.net/' clubTitle='VGDev' clubText='This is VGDEV. I love this club. This is my favorite club. I love clubbing!'/>
+                <ClubLogo logo={boardgames} clubLink='https://www.derivative-calculator.net/' clubTitle='VGDev' clubText="Georgia Tech's premiere board gaming club."/>
                 <ClubLogo logo={sga} clubLink='https://github.com/WreckConOrg/wreckcon-app' clubTitle='Board Games' clubText='I LOVE BOARD GAMING GAMING GAMING GAMING GAMING GAMING GAMING GAMIN!'/>
             </div>
         </div>
