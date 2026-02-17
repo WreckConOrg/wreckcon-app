263a2720fe3e01b49d17fbecc0b28142257c89f9
chase.obrien <chaseobrien128@gmail.com>
2026-02-17 01:13:42 -0500

    2026 artists (no logo :( )

--- src/pages/artists/ArtistInfo.tsx
+++ src/pages/artists/ArtistInfo.tsx
@@ -1,7 +1,6 @@
 import { Desk, Globe, InstagramLogo } from "@phosphor-icons/react";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import foxarcada from "./../../assets/guest_pictures/Foxarcada.png";
 import { isScreenSizeMedium } from "../../utils/BrowserUtils";
 
 export interface ArtistInfoProps {
@@ -15,58 +14,6 @@ export interface ArtistInfoProps {
 }
 
 export const ArtistInfo = (props: ArtistInfoProps) => {
-  // const [hover, setHover] = useState(false);
-  //   <div className="w-[150px] md:w-[300px] bg-[#D9D9D9] flex flex-col items-center rounded-lg">
-  //   <div
-  //     className={`h-12 md:h-20 w-[100%] text-center rounded-t-lg flex flex-col justify-center align-center flex-shrink-0 ${
-  //       colors[(props.index ?? 0) % colors.length]
-  //     }`}
-  //   >
-  //     <span className="font-coolvetica text-lg leading-none md:text-3xl px-4">
-  //       {props.name}
-  //     </span>
-  //   </div>
-
-  //   <div className="flex flex-col md:flex-row w-[100%] gap-1 md:gap-4 p-1 md:p-4">
-  //     <div className="flex flex-row items-center md:px-4 gap-4 align-center">
-  //       <Desk size={iconSize} />
-  //       <span className="font-coolvetica text-lgmd:text-3xl mt-1"> A1</span>
-  //     </div>
-  //     {isScreenSizeMedium && <hr className="h-full w-[2px] bg-black" />}
-  //     <div className="flex flex-row gap-2 md:gap-8 items-end">
-  //       <Link to="https://instagram.com" target="_blank">
-  //         <InstagramLogo size={iconSize} />
-  //       </Link>
-  //       <Globe size={iconSize} />
-  //     </div>
-  //   </div>
-  // </div>
-
-  {
-    /* <div
-        className={`h-12 md:h-20 w-[100%] text-center rounded-t-lg flex flex-col justify-center align-center flex-shrink-0 ${
-          colors[(props.index ?? 0) % colors.length]
-        }`}
-      >
-        <span className="font-coolvetica text-lg leading-none md:text-3xl px-4">
-          {props.name}
-        </span>
-      </div>
-
-      <div className="flex flex-col md:flex-row w-[100%] gap-1 md:gap-4 p-1 md:p-4">
-        <div className="flex flex-row items-center md:px-4 gap-4 align-center">
-          <Desk size={iconSize} />
-          <span className="font-coolvetica text-lgmd:text-3xl mt-1"> A1</span>
-        </div>
-        {isScreenSizeMedium && <hr className="h-full w-[2px] bg-black" />}
-        <div className="flex flex-row gap-2 md:gap-8 items-end">
-          <Link to="https://instagram.com" target="_blank">
-            <InstagramLogo size={iconSize} />
-          </Link>
-          <Globe size={iconSize} />
-        </div>
-      </div> */
-  }
 
   const colors = [
     "bg-[#659470]",
@@ -96,5 +43,5 @@ export const ArtistInfo = (props: ArtistInfoProps) => {
       </div>
     </div>
   );
-  return props.website ? <Link to={props.website}>{content}</Link> : content;
+  return props.socialLink ? <Link to={props.socialLink}>{content}</Link> : content;
 };
