bd7d039a5e9b8bbe5fd6eb87755219e6344d04b4
Aadarsh <avavilikolanu3@gatech.edu>
2026-02-27 15:52:20 -0500

    Update home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,207 +1,56 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { useCountdown } from "../../hooks/countdownTimes";
-import classNames from "classnames";
-import { BrowserView, isMobile, MobileView } from "../../utils/BrowserUtils";
 
 interface HomeProps {
   phraseUsed: string;
 }
 
+const NAV_LINKS = [
+  { name: "Event Info", path: "/info", color: "bg-[#D9D9D9]" },
+  { name: "Schedule", path: "/schedule", color: "bg-[#D9D9D9]" },
+  { name: "Tickets", path: "https://gatech.universitytickets.com/...", color: "bg-[#FFC42D]" },
+];
+
 export const Home = (props: HomeProps): JSX.Element => {
   const CONDATE = new Date("February 28, 2026 10:00:00").getTime();
   const [days, hours, minutes, seconds] = useCountdown(CONDATE);
 
-  const ZeroPad = (n: number) => {
-    return ("0" + n).slice(-2);
-  };
+  const ZeroPad = (n: number) => n.toString().padStart(2, '0');
 
   return (
-    <div>
-      <div className="min-h-screen bg-[#2e2f31] overflow-hidden">
-        <div
-          className={classNames({
-            "bg-darktape bg-contain bg-repeat-x rotate-[-47.11deg] overflow-hidden w-[300vw] left-1/2 translate-x-[-36vw] h-[10vw]":
-              !isMobile,
-          })}
-        />
-        <div
-          className={classNames(
-            {
-              "bg-tape bg-contain bg-repeat-x rotate-[18.55deg] overflow-hidden w-[300vw] left-1/2 translate-x-[-80vw] h-[10vw]":
-                !isMobile,
-            },
-            { hidden: isMobile }
-          )}
-        />
-        <div
-          className={classNames(
-            { "translate-x-[6vw] translate-y-[-1vw] leading-[4vw]": !isMobile },
-            {
-              "h-[100vh] flex flex-col justify-center items-center text-center mt-[-10vh]":
-                isMobile,
-            }
-          )}
-        >
-          <span
-            className={classNames(
-              "font-coolvetica",
-              { "text-[8vw]": !isMobile },
-              { "text-[8.5vh]": isMobile }
-            )}
-          >
-            <span className="text-[#ffc42d]">Wreck</span>
-            <span className="text-white">Con</span>
-          </span>
-          <br className={classNames({ hidden: isMobile })} />
-          <span
-            className={classNames(
-              "font-interbold font-bold text-white",
-              { "text-[3vw]": !isMobile },
-              { "text-[7vw] mt-[2vh]": isMobile }
-            )}
-          >
-            {ZeroPad(days)}:{ZeroPad(hours)}:{ZeroPad(minutes)}:
-            {ZeroPad(seconds)} • 2/28/26
-          </span>
-          <br />
-          <div
-            className={classNames(
-              "font-inter text-white leading-snug",
-              { "text-[2.3vw] w-[45vw]": !isMobile },
-              { "text-[5vw] w-[90vw] mt-[2vh] mb-[3vh]": isMobile }
-            )}
-          >
-            {"WreckCon is " + props.phraseUsed + "."}
-            {/* We appreciate your feedback - please fill out our{" "}
-            <Link
-              className="underline "
-              to="https://tinyurl.com/wreckcon25"
-              target="_blank"
-            >
-              Feedback Form
-            </Link>{" "}
-            to help us improve future WreckCons! */}
-          </div>
+    <div className="min-h-screen bg-[#2e2f31] overflow-x-hidden flex flex-col justify-center relative selection:bg-[#ffc42d] selection:text-black">
+      <div className="hidden md:block absolute inset-0 pointer-events-none overflow-hidden">
+        <div className="bg-darktape bg-contain bg-repeat-x rotate-[-45deg] w-[300vw] absolute -translate-x-[36vw] h-32 top-[10%]" />
+        <div className="bg-tape bg-contain bg-repeat-x rotate-[19deg] w-[300vw] absolute left-1/2 -translate-x-[65vw] h-32 bottom-[20%]" />
+      </div>
+
+      <div className="z-10 flex flex-col items-center text-center px-6 md:items-start md:text-left md:pl-24 md:pt-32">
+        <h1 className="font-coolvetica leading-none">
+          <span className="text-[#ffc42d] text-[18vw] md:text-[8vw]">Wreck</span>
+          <span className="text-white text-[18vw] md:text-[8vw]">Con</span>
+        </h1>
+
+        <div className="font-interbold font-bold text-white text-[7vw] md:text-[3vw] mt-4 md:mt-0">
+          {ZeroPad(days)}:{ZeroPad(hours)}:{ZeroPad(minutes)}:{ZeroPad(seconds)} 
+          <span className="ml-4 opacity-80 whitespace-nowrap">• 2/28/26</span>
         </div>
-        <BrowserView>
-          <div className="flex flex-row gap-4 pl-[6vw] pt-[6vw] mb-[3vh]">
+
+        <p className="font-inter text-white text-[5vw] md:text-[2.3vw] mt-6 md:mt-2 max-w-[90vw] md:max-w-[45vw] leading-snug">
+          WreckCon is {props.phraseUsed}.
+        </p>
+        <nav className="flex flex-col md:flex-row gap-4 mt-12 w-full md:w-auto items-center">
+          {NAV_LINKS.map((link) => (
             <Link
-              className="bg-[#D9D9D9] w-fit h-[4vw] flex items-center justify-center z-10 rounded-lg"
-              to={"/about"}
+              key={link.name}
+              to={link.path}
+              className={`${link.color} w-[60vw] md:w-auto h-[12vw] md:h-[4vw] flex items-center justify-center rounded-lg shadow-lg hover:scale-105 transition-transform`}
             >
-              <div className="font-inter font-bold text-[#2e2f31] text-[2vw] mx-[3vw] ">
-              About
+              <div className="font-inter font-bold text-[#2e2f31] text-[4vw] md:text-[1.8vw] px-[6vw] md:px-[3vw]">
+                {link.name}
               </div>
             </Link>
-            {/* <Link
-              className="bg-[#D9D9D9] w-fit h-[4vw] flex items-center justify-center z-10 rounded-lg"
-              to={"/involved"}
-            >
-              <div className="font-inter font-bold text-[#2e2f31] text-[2vw] mx-[3vw] ">
-              Get Involved
-              </div>
-            </Link> */}
-            <Link
-              className="bg-[#D9D9D9] w-fit h-[4vw] flex items-center justify-center z-10 rounded-lg"
-              to={"/info"}
-            >
-              <div className="font-inter font-bold text-[#2e2f31] text-[2vw] mx-[3vw] ">
-                Event Info
-              </div>
-            </Link>
-            <Link
-              className="bg-[#FFC42D] a w-fit h-[4vw] flex items-center justify-center z-10 rounded-lg"
-              to={"https://gatech.universitytickets.com/w/event.aspx?id=2196"}
-            >
-              <div className="font-inter font-bold text-[#2e2f31] text-[2vw] mx-[3vw] ">
-                Tickets
-              </div>
-            </Link>
-            {/* <Link
-              className="bg-[#D9D9D9] w-fit h-[4vw] flex items-center justify-center z-10 rounded-lg"
-              to={"/events"}
-            >
-              <div className="font-inter font-bold text-[#2e2f31] text-[2vw] mx-[3vw] ">
-                Events
-              </div>
-            </Link> */}
-            {/* <Link
-              className="bg-[#D9D9D9] w-fit h-[4vw] flex items-center justify-center z-10 rounded-lg"
-              to={"/schedule"}
-            >
-              <div className="font-inter font-bold text-[#2e2f31] text-[2vw] mx-[3vw] ">
-                Schedule
-              </div>
-            </Link> */}
-            {/* <Link
-              className="bg-[#D9D9D9] w-fit h-[4vw] flex items-center justify-center z-10 rounded-lg"
-              to={"/guests"}
-            >
-              <div className="font-inter font-bold text-[#2e2f31] text-[2vw] mx-[3vw] ">
-                Guests
-              </div>
-            </Link> */}
-          </div>
-        </BrowserView>
-        <MobileView className="flex flex-col items-center justify-center translate-y-[-30vh]">
-          <Link
-            className="bg-[#D9D9D9] w-[45vw] h-[4vh] mb-[3vh] flex items-center justify-center rounded-md"
-            to={"/about"}
-          >
-            <div className="font-inter font-bold text-[#2e2f31] text-[4vw] mx-[3vw] ">
-              About WreckCon
-            </div>
-          </Link>
-          {/* <Link
-            className="bg-[#D9D9D9] w-[45vw] h-[4vh] mb-[3vh] flex items-center justify-center rounded-md"
-            to={"/involved"}
-          >
-            <div className="font-inter font-bold text-[#2e2f31] text-[4vw] mx-[3vw] ">
-              Get Involved
-            </div>
-          </Link> */}
-          <Link
-            className="bg-[#D9D9D9] w-[45vw] h-[4vh] mb-[3vh] flex items-center justify-center rounded-md"
-            to={"/info"}
-          >
-            <div className="font-inter font-bold text-[#2e2f31] text-[4vw] mx-[3vw]">
-              Event Info
-            </div>
-          </Link>
-          <Link
-            className="bg-[#FFC42D] w-[45vw] h-[4vh] mb-[3vh] flex items-center justify-center rounded-md"
-            to={"https://gatech.universitytickets.com/w/event.aspx?id=2196"}
-          >
-            <div className="font-inter font-bold text-[#2e2f31] text-[4vw] mx-[3vw]">
-              Tickets
-            </div>
-          </Link>
-          {/* <Link
-            className="bg-[#D9D9D9] w-[45vw] h-[4vh] mb-[3vh] flex items-center justify-center rounded-md"
-            to={"/events"}
-          >
-            <div className="font-inter font-bold text-[#2e2f31] text-[4vw] mx-[3vw]">
-              Events
-            </div>
-          </Link> */}
-          {/* <Link
-            className="bg-[#D9D9D9] w-[45vw] h-[4vh] mb-[3vh] flex items-center justify-center rounded-md"
-            to={"/schedule"}
-          >
-            <div className="font-inter font-bold text-[#2e2f31] text-[4vw] mx-[3vw]">
-              Schedule
-            </div>
-          </Link> */}
-          {/* <Link
-            className="bg-[#D9D9D9] w-[45vw] h-[4vh] mb-[3vh] flex items-center justify-center rounded-md"
-            to={"/guests"}
-          >
-            <div className="font-inter font-bold text-[#2e2f31] text-[4vw] mx-[3vw]">
-              Guests
-            </div>
-          </Link> */}
-        </MobileView>
+          ))}
+        </nav>
       </div>
     </div>
   );
