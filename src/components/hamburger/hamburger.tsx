cd292c5ed67c0cbd97f9f11243e57e73db671a6a
Kenneth Doan <doankenneth@gmail.com>
2026-04-15 17:18:53 -0400

    Readded Divider to Hamburger Menu
    - Readded Divider which was accidentally deleted in removing shop

--- src/components/hamburger/hamburger.tsx
+++ src/components/hamburger/hamburger.tsx
@@ -20,7 +20,7 @@ interface NavConfig {
 const NAV_LINKS: NavConfig[] = [
   { item: NavBarItemEnum.ABOUT, label: "about", url: "/about", hasDivider: true },
   { item: NavBarItemEnum.TICKETS, label: "tickets", url: "https://gatech.universitytickets.com/w/event.aspx?id=2196" },
-  { item: NavBarItemEnum.DISCORD, label: "discord", url: "https://discord.gg/BY2hTWjz4s" },
+  { item: NavBarItemEnum.DISCORD, label: "discord", url: "https://discord.gg/BY2hTWjz4s", hasDivider: true },
   { item: NavBarItemEnum.EVENT_INFO, label: "event info", url: "/info" },
   { item: NavBarItemEnum.SCHEDULE, label: "schedule", url: "/schedule" },
   { item: NavBarItemEnum.BROCHURE, label: "brochure", url: "/brochure" },
